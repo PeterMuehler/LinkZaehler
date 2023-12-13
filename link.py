cd52bd8a5bdd8a7a0b55d5ae8242686d2481dd8c
PeterMuehler <john.doe.business@gmx.de>
2023-12-13 23:36:32 +0100

    Update link.py
    Overall overworked

--- link.py
+++ link.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
+import datetime;
+ct = datetime.datetime.now()
 
 seitenzahl = input("Bitte die maximale Seitenanzahl eingeben.")
 link = input("Bitte den Seitenlink eingeben und die Seitenzahl durch '%a' (ohne '')ersetzen. ")
 a = 1
 
-while a <= int(seitenzahl):
-	string = link % (a)
-	print (string)
-	a = a + 1
+
+with open("output.txt", "a") as f:
+    print (ct, file=f)
+    while a <= int(seitenzahl):
+        string = link % (a)
+        print (string, file=f)
+        a = a + 1
+    print ("--------------------", file=f)
+    print ("", file=f)
+    print ("", file=f)
+    print ("", file=f)
